2c5ab747d40bdeb8aa59a36a867566cfa4d4689e
AKhoriby <ahmedelkhoriby010@gmail.com>
2025-05-04 00:03:28 +0300

    feat: implement Zod schema validation for requests

---
+++ middlewares/validate.ts
@@ -0,0 +1,20 @@
+import type { Request, Response, NextFunction } from 'express';
+import type { AnyZodObject } from 'zod';
+import { ZodError } from 'zod';
+
+export const validate =
+  (schema: AnyZodObject) =>
+  (req: Request, res: Response, next: NextFunction) => {
+    try {
+      const parsedData = schema.parse(req.body);
+      req.body = parsedData.body;
+
+      return next();
+    } catch (error: ZodError | any) {
+      if (error instanceof ZodError) {
+        res.status(400).json({ success: false, errors: error.errors });
+        return;
+      }
+      res.status(400).json({ success: false, errors: error });
+    }
+  };
--- routes/restaurants.ts
+++ routes/restaurants.ts
@@ -1,8 +1,11 @@
 import express from 'express';
+import { validate } from '../middlewares/validate.js';
+import { RestaurantSchema, type Restaurant } from '../schemas/restaurant.js';
 const router = express.Router();
 
-router.get('/', async (req, res) => {
-  res.send('Hello world');
+router.post('/', validate(RestaurantSchema), async (req, res) => {
+  const data = req.body as Restaurant;
+  res.send('Restaurant created successfully!');
 });
 
 export default router;
